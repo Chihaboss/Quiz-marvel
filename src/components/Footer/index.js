bafaf03b537f119dc16ced9468be6eae2cbc991a
unknown <chihab.che@gmail.comgit config --global user.name>
2022-07-16 04:20:18 +0100

    first commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+            <div className="footer-container">
+                <p>Projet réalisé par Chihab-eddine Essaim</p>
+                <p>projet de fin de module</p>
+            </div>
+        </footer>
+  )
+}
+
+export default Footer
