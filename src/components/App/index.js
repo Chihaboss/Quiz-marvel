33d05cf400a04dbff237f6b77aff122450f1338a
unknown <chihab.che@gmail.comgit config --global user.name>
2022-07-20 16:34:58 +0100

    mythird

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,16 +1,34 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from '../Header';
 import Landing from '../Landing';
 import Footer from '../Footer';
+import Welcome from '../Welcome';
+import Login from '../Login';
+import Signup from '../Signup';
+import ErrorPage from '../ErrorPage';
 import '../../App.css';
 
 function App() {
   return (
-    <div>
-      <Header></Header>
-      <Landing/>
+    
+      <Router>
+        <Header/>
+        <Routes>
+          <Route  exact path="/" element={<Landing/>} />
+          <Route  path="/welcome" element={<Welcome/>} />
+          <Route  path="/login" element={<Login/>} />
+          <Route  path="/signup" element={<Signup/>} />
+          <Route element={ErrorPage} />
+        </Routes>
+      
+      
+    
+     
+  
+      
       <Footer/>
-    </div>
+      </Router>
   );
 }
 
