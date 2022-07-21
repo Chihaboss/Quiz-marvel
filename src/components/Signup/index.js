9fc3b0ae6631fbccf3e7d89fbd83b79b39a91a1f
unknown <chihab.che@gmail.comgit config --global user.name>
2022-07-21 08:45:42 +0100

    my5th

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -1,6 +1,12 @@
-import React, {useState} from 'react'
+import React, {useState, useContext} from 'react'
+import {FirebaseContext} from '../Firebase'
+import { Link, useNavigate } from 'react-router-dom';
 
-const Signup = () => {
+const Signup = (props) => {
+
+  const navigate = useNavigate();
+
+  const firebase = useContext(FirebaseContext);
   
 const data = {
   psuedo: '',
@@ -10,9 +16,30 @@ const data = {
 }
 
 const [loginData, setLoginData] = useState(data)
+const [error, setError] = useState('')
 
 const handleChange = e => {
   setLoginData({...loginData, [e.target.id]: e.target.value});
+}
+
+const handleSubmit = e => {
+  e.preventDefault();
+  const { email, password, pseudo } = loginData;
+  firebase.signupUser(email, password)
+  .then( authUser => {
+      return firebase.user(authUser.user.uid).set({
+          pseudo,
+          email
+      })
+  })
+  .then(() => {
+      setLoginData({...data});
+      navigate('/welcome')
+  })
+  .catch(error => {
+      setError(error);
+      setLoginData({...data});
+  })
 }
  
 const {psuedo, email, password, confirmPassword} = loginData;
@@ -21,7 +48,8 @@ const btn = psuedo ==='' || email === '' || password === '' || password !== conf
 <button disabled>Inscription</button> : <button>Inscription</button>
   
   
-  
+   // gestion erreurs
+   const errorMsg = error !== '' && <span>{error.message}</span>;
   
   
   return (
@@ -32,9 +60,9 @@ const btn = psuedo ==='' || email === '' || password === '' || password !== conf
           </div>
          <div className='formBoxRight'>
           <div className='formContent'>
-           
+           {errorMsg}
           <h2>Inscription</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
               
               <div className='inputBox'>
                 <input onChange={handleChange} value={psuedo} type="text" id='psuedo' autoComplete='off' required/>
@@ -58,6 +86,9 @@ const btn = psuedo ==='' || email === '' || password === '' || password !== conf
               </div>
               {btn}
             </form>
+            <div className='linkContainer'>
+            <Link className='simpleLink' to='/login'>Deja inscrit? connectez vous</Link>
+            </div>
           </div>
           </div> 
         </div>
